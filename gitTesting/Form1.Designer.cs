f48341edb051a4d65a911bb3a0b20a7afce645f9
orlali <64191879+orlali@users.noreply.github.com>
2020-04-28 19:47:59 +1200

    button implemented

--- gitTesting/Form1.Designer.cs
+++ gitTesting/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSayHi = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSayHi
+            // 
+            this.btnSayHi.Location = new System.Drawing.Point(162, 198);
+            this.btnSayHi.Name = "btnSayHi";
+            this.btnSayHi.Size = new System.Drawing.Size(121, 61);
+            this.btnSayHi.TabIndex = 0;
+            this.btnSayHi.Text = "Say Hi";
+            this.btnSayHi.UseVisualStyleBackColor = true;
+            this.btnSayHi.Click += new System.EventHandler(this.btnSayHi_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(162, 310);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 20);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnSayHi);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSayHi;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
--- gitTesting/Form1.cs
+++ gitTesting/Form1.cs
@@ -16,5 +16,10 @@ namespace gitTesting
         {
             InitializeComponent();
         }
+
+        private void btnSayHi_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "Hello Git World!";
+        }
     }
 }
